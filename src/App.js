69262067b50540b6d3e1cab9c2a11e98b7e38e33
Aaron Anderson <aaronlynanderson@gmail.com>
2019-05-18 16:18:15 -0400

    adds wyre buttons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Fortmatic from "fortmatic";
 // Works for web3 1.0 and pre-1.0 versions
 import Web3 from "web3";
 
+require("dotenv").config();
+
 const fm = new Fortmatic("pk_test_4D175B5BE5D1ACA4");
 let web3 = new Web3(fm.getProvider());
 
